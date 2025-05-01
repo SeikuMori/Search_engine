4eda525469debb1144d6fcd2a58fb98d51fcfdba
OneHeart <ryuu_awai@mail.ru>
2025-05-01 10:53:33 +0500

    Remove .vscode and build directories from git tracking

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/OneHeart/.vscode/projects/search_engine/build/_deps/googletest-src/googlemock
-# Build directory: C:/Users/OneHeart/.vscode/projects/search_engine/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/OneHeart/.vscode/projects/search_engine/tests
-# Build directory: C:/Users/OneHeart/.vscode/projects/search_engine/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/OneHeart/.vscode/projects/search_engine/build/tests/search_engine_tests[1]_include.cmake")
